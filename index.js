73b015e676ac34fe24f7147f946a803c7a23bf52
Simon Mullaney <simonpmullaney@gmail.com>
2022-04-28 12:17:46 +0100

    Removing additonal consoles

--- index.js
+++ index.js
@@ -14,20 +14,11 @@ Find a 4-byte prefix so that, a SHA256 hash of the prefix combined with the orig
 
   let str = "f064b8b61422a3456cb273a474a1fb0cabb04200a6a82a9426bd01f56c97fbf8c4ef58634fd5cf21af29e7db3406de4f886fe71408696789f853af9932a84b79";
 
-  let condition = false;
-
   let randByte = crypto.randomBytes(4);
   let strRandBytes = randByte.toString('hex');
   let concat = strRandBytes + str;
 
   let sha256Concat = crypto.createHash('sha256').update(concat).digest('hex');
-
-  // console.log("sha256Concat",sha256Concat);
-  // console.log(sha256Concat.length);
-  // console.log(sha256Concat[60]);
-  // console.log(sha256Concat[61]);
-  // console.log(sha256Concat[62]);
-  // console.log(sha256Concat[63]);
 
   while (!((sha256Concat[60]=="c") && (sha256Concat[61]=="a") && (sha256Concat[62]=="f" ) && (sha256Concat[63]=="e"))) {
 
